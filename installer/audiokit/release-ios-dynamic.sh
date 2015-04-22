92dde26a0c6e4fdb9d00bd2941053a2e194a7a1d
Stephane Peter <megastep@megastep.org>
2015-04-21 18:09:13 -0700

    Generates working CsoundLib.framework on iOS!

--- installer/audiokit/release-ios-dynamic.sh
+++ installer/audiokit/release-ios-dynamic.sh
@@ -17,8 +17,11 @@
 AK_ROOT=${AK_ROOT:-$PWD/../../../AudioKit/AudioKit/Platforms/iOS}
 BUILD_TYPE=${BUILD_TYPE:-Release}
 
+# Path to the audiokit/libsndfile library with built libraries
+SNDFILE=$HOME/src/ak/libsndfile
+
 # Use xcpretty to beautify xcodebuild output if it is available
-if which xcpretty 2>/dev/null; then
+if which xcpretty >/dev/null 2>&1; then
    XCPRETTY=xcpretty
 else
    XCPRETTY=cat
@@ -37,17 +40,21 @@ cd ios
 FLAGS="-DUSE_GETTEXT=0 -DUSE_DOUBLE=0 -DUSE_OPEN_MP=0 \
 	-DBUILD_STATIC_LIBRARY=0 -DBUILD_CSOUND_AC=1 -DBUILD_RELEASE=1 -DBUILD_TESTS=0 \
 	-DBUILD_CSOUND_AC_PYTHON_INTERFACE=0 -DBUILD_CSOUND_AC_LUA_INTERFACE=0 \
-	-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DUSE_CURL=0 -DBUILD_IMAGE_OPCODES=0 -DIOS=1"
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DUSE_CURL=0 -DBUILD_IMAGE_OPCODES=0 -DIOS=1 -DSNDFILE_DIR=$SNDFILE"
 
 echo "Building Csound (float) for ${BUILD_TYPE} ..."
 echo "Using flags: $FLAGS"
 
-#cmake ../../.. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../iOS.cmake -DUSE_GETTEXT=0 -DUSE_DOUBLE=0 -DBUILD_STATIC_LIBRARY=1 -DBUILD_CSOUND_AC=1 -DBUILD_RELEASE=1 -DBUILD_TESTS=0 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DUSE_CURL=0 -DBUILD_IMAGE_OPCODES=0 || exit 1
 cmake ../../.. -G Xcode $FLAGS || exit 1
-#(xcodebuild -sdk iphoneos -xcconfig ../device.xcconfig -target CsoundLib -configuration $BUILD_TYPE | $XCPRETTY ) || exit 1
+(xcodebuild -sdk iphoneos -xcconfig ../device.xcconfig -target CsoundLib -configuration $BUILD_TYPE | $XCPRETTY ) || exit 1
+cp $BUILD_TYPE/CsoundLib.framework/CsoundLib CsoundLib-dev.dylib
 (xcodebuild -sdk iphonesimulator -xcconfig ../simulator.xcconfig -target CsoundLib -configuration $BUILD_TYPE | $XCPRETTY ) || exit 1
-#lipo -create libcsound-device.a $BUILD_TYPE/libCsoundLib.a -output ../libcsound.a || exit 1
+cp $BUILD_TYPE/CsoundLib.framework/CsoundLib CsoundLib-sim.dylib
 
-# Copy new libraries and headers for Csound and its opcodes to the AudioKit framework
+lipo -create CsoundLib-dev.dylib CsoundLib-sim.dylib -output $BUILD_TYPE/CsoundLib.framework/CsoundLib || exit 1
+install_name_tool -change `otool -LX CsoundLib |grep libsndfile.1|awk '{print $1}'` @rpath/CsoundLib.framework/libs/libsndfile.1.dylib $BUILD_TYPE/CsoundLib.framework/CsoundLib || exit 1
+
+# Copy new libraries for Csound to the AudioKit framework for iOS
+cp $BUILD_TYPE/CsoundLib.framework/CsoundLib $AK_ROOT/CsoundLib.framework/
 
 echo "... finished."
